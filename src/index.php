68747ac9acfe3d01ab0c405677bad72dee58f0bb
Borja Gómez <borgomrui@alu.edu.gva.es>
2023-11-09 19:21:41 +0100

    prueba

--- src/index.php
+++ src/index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<<<<<<< HEAD
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -51,4 +52,29 @@
     </script>
     
 </body>
+=======
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hola IES MRE</title>
+        <style>
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 80%;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="hello.gif" alt="Hola" class="center" style="width:20%;height:20%;">
+
+        <h1><?php echo "Hola Mundo des del IES MRE"; ?></h1>
+        <p>
+        <?php
+            echo "Hoy estamos a " . date("d/m/Y");
+        ?>
+        </p>
+    </body>
+>>>>>>> e7eba53 (prueba)
 </html>
